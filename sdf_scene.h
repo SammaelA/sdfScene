48ed4da5d59dde50a3f8cbeb95a1da7a04127d6a
Sammael <albgar-14@yandex.ru>
2024-03-25 15:51:24 +0300

    making SDF scene ready for slicer 2

--- sdf_scene.h
+++ sdf_scene.h
@@ -1,50 +1,7 @@
 #pragma once
-#include "LiteMath/LiteMath.h"
+#include "sdf_scene_gpu.h"
 #include <string>
-#include <vector>
 
-// enum SdfPrimitiveType
-static constexpr unsigned SDF_PRIM_SPHERE = 0;
-static constexpr unsigned SDF_PRIM_BOX = 1;
-static constexpr unsigned SDF_PRIM_CYLINDER = 2;
-static constexpr unsigned SDF_PRIM_SIREN = 3;
-
-struct SdfObject
-{
-  unsigned type;          // from enum SdfPrimitiveType
-  unsigned params_offset; // in parameters vector
-  unsigned params_count;
-  unsigned neural_id = 0; // index in neural_properties if type is neural
-  float distance_mult = 1.0f;
-  float distance_add = 0.0f;
-  LiteMath::float3 max_pos;
-  LiteMath::float3 min_pos;
-  LiteMath::float4x4 transform;
-  unsigned complement = 0; // 0 or 1
-};
-struct SdfConjunction
-{
-  unsigned offset; // in objects vector
-  unsigned size;
-  LiteMath::float3 max_pos;
-  LiteMath::float3 min_pos;
-};
-
-constexpr int NEURAL_SDF_MAX_LAYERS = 8;
-constexpr int NEURAL_SDF_MAX_LAYER_SIZE = 1024;
-constexpr float SIREN_W0 = 30;
-struct NeuralProperties
-{
-  struct DenseLayer
-  {
-    unsigned offset;
-    unsigned in_size;
-    unsigned out_size;
-  };
-
-  unsigned layer_count;
-  DenseLayer layers[NEURAL_SDF_MAX_LAYERS];
-};
 
 struct SdfScene
 {
@@ -97,36 +54,6 @@ struct SdfSceneView
   unsigned conjunctions_count;
   unsigned neural_properties_count;
 };
-
-struct SdfHit
-{
-  unsigned hit_id = 0; // 0 if no hit
-  LiteMath::float3 hit_pos;
-  LiteMath::float3 hit_norm;
-};
-
-// evaluate distance to a specific primitive in scene
-float eval_dist_prim(const float *parameters, const SdfObject *objects, const SdfConjunction *conjunctions, const NeuralProperties *neural_properties,
-                     unsigned parameters_count, unsigned objects_count, unsigned conjunctions_count, unsigned neural_properties_count,
-                     unsigned prim_id, LiteMath::float3 p);
-
-// evaluate distance to a specific conjunction (a single primitive or intersection of primitives)
-float eval_dist_conjunction(const float *parameters, const SdfObject *objects, const SdfConjunction *conjunctions, const NeuralProperties *neural_properties,
-                            unsigned parameters_count, unsigned objects_count, unsigned conjunctions_count, unsigned neural_properties_count,
-                            unsigned conj_id, LiteMath::float3 p);
-
-// evaluate distance to a whole scene (minimum of distances to all conjunctions)
-float eval_dist_scene(const float *parameters, const SdfObject *objects, const SdfConjunction *conjunctions, const NeuralProperties *neural_properties,
-                      unsigned parameters_count, unsigned objects_count, unsigned conjunctions_count, unsigned neural_properties_count,
-                      LiteMath::float3 p);
-
-// perform sphere tracing to find ray intersection with a specific conjunction and inside given bbox
-// (it can be smaller than real bbox of conjunction). Use with acceleration structure on conjunction bboxes
-// dir vector MUST be normalized
-SdfHit sdf_conjunction_sphere_tracing(const float *parameters, const SdfObject *objects, const SdfConjunction *conjunctions, const NeuralProperties *neural_properties,
-                                      unsigned parameters_count, unsigned objects_count, unsigned conjunctions_count, unsigned neural_properties_count,
-                                      unsigned conj_id, const LiteMath::float3 &min_pos, const LiteMath::float3 &max_pos,
-                                      const LiteMath::float3 &pos, const LiteMath::float3 &dir, bool need_norm = false);
 
 // perform sphere tracing to find ray intersection with a whole scene and inside given bbox
 // dir vector MUST be normalized
