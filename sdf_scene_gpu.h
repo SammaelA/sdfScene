61c5257deb4c273c97e9282942d2d4a63001aae0
Sammael <albgar-14@yandex.ru>
2024-03-25 16:26:21 +0300

    making SDF scene ready for slicer 3

--- sdf_scene_gpu.h
+++ sdf_scene_gpu.h
@@ -3,10 +3,23 @@
 #include "LiteMath/LiteMath.h"
 #include <vector>
 
-using LiteMath::float4x4;
 using LiteMath::float2;
 using LiteMath::float3;
 using LiteMath::float4;
+using LiteMath::uint2;
+using LiteMath::uint3;
+using LiteMath::uint4;
+using LiteMath::int2;
+using LiteMath::int3;
+using LiteMath::int4;
+using LiteMath::float4x4;
+using LiteMath::float3x3;
+using LiteMath::cross;
+using LiteMath::dot;
+using LiteMath::length;
+using LiteMath::normalize;
+using LiteMath::to_float4;
+using LiteMath::to_float3;
 
 // enum SdfPrimitiveType
 static constexpr unsigned SDF_PRIM_SPHERE = 0;
